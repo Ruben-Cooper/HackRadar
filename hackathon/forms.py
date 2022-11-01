78de6506315adef626fd426b725bfb8c5822aacd
SkaarsOfficial <skaars1021@gmail.com>
2022-11-01 19:02:46 +1000

    Complete front end of Create Event

--- hackathon/forms.py
+++ hackathon/forms.py
@@ -1,6 +1,6 @@
 
 from flask_wtf import FlaskForm
-from wtforms.fields import TextAreaField,SubmitField, StringField, PasswordField
+from wtforms.fields import *
 from wtforms.validators import InputRequired, Length, Email, EqualTo
 
 
@@ -24,3 +24,14 @@ class RegisterForm(FlaskForm):
     contact_number = StringField("Contact Number", validators=[InputRequired("Please Enter Contact Number")])
     # Address field
     address = StringField("Address", validators=[InputRequired("Please Enter Address")])
+
+class CreateEventForm(FlaskForm):
+    event_name = StringField("Event Name", validators=[InputRequired("Please Enter Event Name")])
+    event_description = TextAreaField("Event Description", validators=[InputRequired("Please Enter Event Description")])
+    event_date = DateField("Event Date", validators=[InputRequired("Please Enter Event Date")])
+    event_image = FileField("Event Image", validators=[InputRequired("Please Enter Event Image")])
+    event_location = StringField("Event Location", validators=[InputRequired("Please Enter Event Location")])
+    ticket_quantity = IntegerField("Ticket Quantity", validators=[InputRequired("Please Enter Ticket Quantity")])
+    ticket_price = DecimalField("Ticket Price", validators=[InputRequired("Please Enter Ticket Price")])
+    event_category = SelectField("Event Category", validators=[InputRequired("Please Enter Event Category")], choices=[('Business Case Competition'), ('Business Proposals'), ('Coding Competition'), ('Datathon'), ('Hackathon'), ('Idea Pitch'), ('Robotics'), ('Seminars')])
+    submit = SubmitField("Create Event")
--- hackathon/views.py
+++ hackathon/views.py
@@ -1,6 +1,6 @@
 from logging import PlaceHolder
 from flask import Blueprint, request, render_template
-from .forms import RegisterForm
+from .forms import RegisterForm, CreateEventForm
 
 
 bp = Blueprint('main', __name__)
@@ -20,6 +20,9 @@ def register():
         print(f"Username: {regform.user_name.data}, Email: {regform.email_id.data}, Password: {regform.password.data}, Contact Number: {regform.contact_number.data}, Address: {regform.address.data}")
     return render_template('register.html', form=regform)
 
-@bp.route('/create_event')
+@bp.route('/create_event', methods=['GET', 'POST'])
 def create_event():
-    return render_template('create_event.html')
+    createform = CreateEventForm()
+    if createform.validate_on_submit():
+        print(f"Event Name: {createform.event_name.data}, Event Description: {createform.event_description.data}, Event Date: {createform.event_date.data}, Event Image: {createform.event_image.data}, Event Location: {createform.event_location.data}, Ticket Quantity: {createform.ticket_quantity.data}, Ticket Price: {createform.ticket_price.data}, Event Category: {createform.event_category.data}")
+    return render_template('create_event.html', form=createform)
