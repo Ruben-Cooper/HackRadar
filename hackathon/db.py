dd770b65cf19470e7bfd17b5c554efb9d42ba5ad
CosmicTG <gustavo.almamorim123@gmail.com>
2022-11-02 11:35:07 +1000

    Update db.py

--- hackathon/db.py
+++ hackathon/db.py
@@ -24,6 +24,7 @@ class User(db.Model):
     phone = db.Column(db.String(9), nullable=False)
     address = db.Column(db.String(100), nullable=False)
 
+
 class Event(db.Model):
     __tablename__ = 'eventdetails'
     event_id = db.Column(db.Integer, primary_key=True)
