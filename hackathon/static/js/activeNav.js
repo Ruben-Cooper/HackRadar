7dd394074afa0b24d61a56541ed56b9b2f4c1923
SkaarsOfficial <skaars1021@gmail.com>
2022-11-03 21:37:30 +1000

    Fixed active nav

--- hackathon/static/js/activeNav.js
+++ hackathon/static/js/activeNav.js
@@ -1,11 +1,9 @@
 const currentPage = location.href;
-const navItem = document.querySelectorAll('.navbar li a');
+const navItem = document.querySelectorAll(".navbar ul a");
 const navLength = navItem.length;
 
 for(var i = 0; i < navLength; i++){
     if(navItem[i].href === currentPage){
         navItem[i].className = "active";
-    }
-    else if (navItem[i].href === "dropdown"){
     }
 }
