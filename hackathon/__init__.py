8ba7f0d60c2591826898694c1aa3987498bd258d
Ruben <ruben.cooper03@gmail.com>
2022-11-05 14:28:26 +1000

    Updated create_event page and database

--- hackathon/__init__.py
+++ hackathon/__init__.py
@@ -3,7 +3,6 @@ from flask import Flask
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from .views import page_not_found
 
 
 # create a function that creates a web application
@@ -23,7 +22,8 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sitedata.sqlite'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['TRAP_HTTP_EXCEPTIONS'] = True
-    app.register_error_handler(Exception, page_not_found)
+    from .views import page_not_found
+    # app.register_error_handler(Exception, page_not_found)
 
     # # initialize db with flask app
     db.init_app(app)
@@ -52,5 +52,4 @@ def create_app():
 
     from . import auth
     app.register_blueprint(auth.bp)
-
     return app
--- hackathon/views.py
+++ hackathon/views.py
@@ -1,17 +1,11 @@
-from logging import PlaceHolder
-from flask import Blueprint, request, render_template
 from .forms import RegisterForm, CreateEventForm, LoginForm, BookEventForm
-from flask import Flask, render_template, request, redirect, url_for, request, abort
+from flask import Flask, render_template, request, redirect, url_for, abort, Blueprint
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import login_user, login_required, logout_user
-
+from flask_login import current_user, login_user, login_required, logout_user
+from . import db
+from .models import User, Event
 
 bp = Blueprint('main', __name__)
-
-
-@bp.errorhandler(404)
-def page_not_found(e):
-    return render_template('error.html'), 404
 
 
 @bp.route('/')
@@ -24,16 +18,13 @@ def index():
 @bp.route('/create_event', methods=['GET', 'POST'])
 @login_required  # Decorator to protect the route from unauthenticated users
 def create_event():
-    createform = CreateEventForm()
-    if createform.validate_on_submit():
-        print(f"Event Name: {createform.event_name.data}, Event Description: {createform.event_description.data}, Event Date: {createform.event_date.data}, Event Image: {createform.event_image.data}, Event Location: {createform.event_location.data}, Ticket Quantity: {createform.ticket_quantity.data}, Ticket Price: {createform.ticket_price.data}, Event Category: {createform.event_category.data}")
-    return render_template('create_event.html', form=createform)
-
-
-@bp.route('/booking_history')
-@login_required
-def booking_history():
-    return render_template('booking_history.html')
+    form = CreateEventForm()
+    if request.method == 'POST' and form.validate():
+        new_event = Event(event_name=form.event_name.data, description=form.event_description.data, date=form.event_date.data, online=form.online_event.data, location=form.event_location.data,
+                          category=form.event_category.data, status=form.event_status.data, tickets_amt=form.ticket_quantity.data, price=form.ticket_price.data, image=form.event_image.data, user_id=current_user.id)
+        db.session.add(new_event)
+        db.session.commit()
+    return render_template('create_event.html', form=form)
 
 
 @bp.route('/event/', methods=['GET', 'POST'])
@@ -49,20 +40,21 @@ def coding_competition():
     return render_template('coding_competition.html')
 
 
-
 @bp.errorhandler(400)
 def page_not_found(e):
     return render_template('error.html'), 404
+
 
 @bp.errorhandler(500)
 def internal_server_error(e):
     return render_template('error.html'), 500
 
+
 @bp.errorhandler(403)
 def forbidden(e):
     return render_template('error.html'), 403
 
+
 @bp.errorhandler(410)
 def gone(e):
     return render_template('error.html'), 410
-
